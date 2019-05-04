7ffbdd3a85803fafe725d2662f9d11c1043a7f91
Giancarlos <giancarlos.isasi@gmail.com>
2019-05-04 18:44:42 -0500

    Setup monorepo style with lerna

---
+++ packages/nexus-scripts/__tests__/nexus-scripts.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const nexusScripts = require('..');
+
+describe('nexus-scripts', () => {
+    it('needs tests');
+});
---
+++ packages/nexus-scripts/lib/nexus-scripts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = nexusScripts;
+
+function nexusScripts() {
+    // TODO
+}
