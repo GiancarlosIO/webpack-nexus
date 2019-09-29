d286b917ccae894e6660bd91ad81063637f3cd36
Giancarlos <giancarlos.isasi@gmail.com>
2019-09-29 18:26:43 -0500

    webpack-nexus: Install eslint dependencies before to copy files

---
+++ packages/webpack-nexus/lib/templateFiles/getEslintTemplate.js
@@ -0,0 +1,9 @@
+function getEslintTemplate() {
+  return `
+module.exports = {
+  extends: 'eslint-config-webpack-nexus',
+}
+`;
+}
+
+module.exports = getEslintTemplate;
--- packages/webpack-nexus/lib/webpack-nexus.js
+++ packages/webpack-nexus/lib/webpack-nexus.js
@@ -15,6 +15,7 @@ const getPackageJsonTemplate = require('./templateFiles/getPackageJsonTemplate')
 const getGitIgnoreContent = require('./templateFiles/getGitIgnoreTemplate');
 const getHtmlTemplate = require('./templateFiles/getHtmlTemplate');
 const getMainJsTempalte = require('./templateFiles/getMainJsTemplate');
+const getEslintTemplate = require('./templateFiles/getEslintTemplate');
 
 // 1. accept the name of the project like second param
 const argv = parseArgs(process.argv.slice(2), {
@@ -69,9 +70,30 @@ installNpmPackages({
   areDevDependencies: false,
 });
 
-// we need to install it to run the linters in a npm command
+// we need to install the dependencies of the eslint plugin
+const eslintPackages = {
+  '@typescript-eslint/eslint-plugin': '^1.12.0',
+  '@typescript-eslint/parser': '^1.12.0',
+  'confusing-browser-globals': '^1.0.9',
+  eslint: '^5.3.0',
+  'eslint-config-airbnb': '^17.1.1',
+  'eslint-config-prettier': '^6.0.0',
+  'eslint-plugin-import': '^2.18.0',
+  'eslint-plugin-jsx-a11y': '^6.2.3',
+  'eslint-plugin-prettier': '^3.0.0',
+  'eslint-plugin-react': '^7.14.2',
+  'eslint-plugin-react-hooks': '^1.6.1',
+  prettier: '^1.15.3',
+  typescript: '^3.5.3',
+};
+const eslintPackagesString = Object.keys(eslintPackages).reduce(
+  (packages, packageKey) => {
+    return `${packages} ${packageKey}@${eslintPackages[packageKey]}`;
+  },
+  '',
+);
 installNpmPackages({
-  packages: 'eslint typescript',
+  packages: `eslint-config-webpack-nexus ${eslintPackagesString}`,
   path: folderPath,
   areDevDependencies: true,
 });
@@ -102,3 +124,21 @@ console.log(
  * - .eslintrc.js
  * - .eslintignore
  */
+// ======================== .eslintrc.js ========================= //
+const eslint = getEslintTemplate();
+const eslintPath = path.join(folderPath, '.eslintrc.js');
+fs.writeFileSync(eslintPath, eslint);
+
+// ======================== .eslintignore ========================= //
+const eslintignore = `
+!.*.js
+!.*.ts
+!.*.tsx
+node_modules/
+coverage/
+dist/
+build/
+`;
+const eslintignorePath = path.join(folderPath, '.eslintignore');
+fs.writeFileSync(eslintignorePath, eslintignore);
+console.log(chalk.green(`> Success to create the eslint config files`));
