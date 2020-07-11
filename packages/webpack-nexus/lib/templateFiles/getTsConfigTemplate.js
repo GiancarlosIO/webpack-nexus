57ad2974dcc296bfc4aa37a856851ba122568904
Giancarlos <giancarlos.isasi@gmail.com>
2020-07-10 22:36:19 -0500

    Update tsConfig template, include libs options to include types for dom

--- packages/webpack-nexus/lib/templateFiles/getTsConfigTemplate.js
+++ packages/webpack-nexus/lib/templateFiles/getTsConfigTemplate.js
@@ -1,27 +1,22 @@
 function getTsConfigTemplate() {
   return `{
   "compilerOptions": {
-    "module": "esNext",
-    // Target latest version of ECMAScript.
-    "target": "esnext",
-    // Search under node_modules for non-relative imports.
     "moduleResolution": "node",
-    // Process & infer types from .js files.
     "allowJs": true,
-    // Don't emit; allow Babel to transform files.
     "noEmit": true,
-    // Enable strictest settings like strictNullChecks & noImplicitAny.
     "strict": true,
-    // Disallow features that require cross-file information for emit.
     "isolatedModules": true,
-    // Import non-ES modules as default imports.
     "esModuleInterop": true,
-    // Avoid passing optional types if this return null
     "strictNullChecks": true,
     "experimentalDecorators": true,
     "noUnusedLocals": true,
     "noUnusedParameters": true,
     "noImplicitAny": false,
+    "lib": [
+      "dom",
+      "dom.iterable",
+      "esnext"
+    ],
     "jsx": "react",
     "baseUrl": "./",
     "paths": {
@@ -36,7 +31,7 @@ function getTsConfigTemplate() {
     ]
   },
   "include": [
-    "src/**/*"
+    "src/*"
   ],
   "exclude": [
     "node_modules",
