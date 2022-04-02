110433fdf94acb1f488a47354fdeed1559e3f2c5
Giancarlos Isasi <giancarlos@crehana.com>
2022-04-02 15:52:00 -0500

    feat: improve linters configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('./packages/eslint-config-webpack-nexus/lib/eslint-config-webpack-nexus.js');
+module.exports = {
+  extends: './packages/eslint-config-webpack-nexus/lib/eslint-config-webpack-nexus.js',
+  root: true,
+  // "parserOptions": {
+  //   project: ['./tsconfig.eslint.json']
+  // },
+};
