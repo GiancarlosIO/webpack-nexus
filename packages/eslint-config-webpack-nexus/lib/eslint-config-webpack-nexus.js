61214b183638a82f2878992326bf96d7282078be
Giancarlos Isasi <giancarlos@crehana.com>
2022-04-05 19:38:49 -0500

    feat: disable import/no-unresolved eslint rule because typescript will handle this

--- packages/eslint-config-webpack-nexus/lib/eslint-config-webpack-nexus.js
+++ packages/eslint-config-webpack-nexus/lib/eslint-config-webpack-nexus.js
@@ -80,6 +80,7 @@ module.exports = {
     'global-require': 0,
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
     import: 0,
     'func-names': 0,
     'space-before-function-paren': 0,
